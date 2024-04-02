32cde14cad73125fe340f1eb091719530e2bf18f
Sebastian Barnowski <sebastian@npe-inc.com>
2024-04-02 17:30:25 -0600

    Add ZFILE and ZDATA events

--- lib/src/zmodem_event.dart
+++ lib/src/zmodem_event.dart
@@ -1,5 +1,6 @@
 import 'package:zmodem/src/util/debug.dart';
 import 'package:zmodem/src/zmodem_fileinfo.dart';
+import 'package:zmodem/src/zmodem_frame.dart';
 
 abstract class ZModemEvent {}
 
@@ -20,8 +21,9 @@ class ZFileOfferedEvent implements ZModemEvent {
 /// A chunk of data of the file currently being received.
 class ZFileDataEvent implements ZModemEvent {
   final List<int> data;
+  final ZModemDataPacket packet;
 
-  ZFileDataEvent(this.data);
+  ZFileDataEvent(this.data, this.packet);
 
   @override
   String toString() {
@@ -38,6 +40,35 @@ class ZFileEndEvent implements ZModemEvent {
     return 'ZFileEndEvent()';
   }
 }
+class ZFileEvent implements ZModemEvent {
+  final int p0;
+  final int p1;
+  final int p2;
+  final int p3;
+  ZFileEvent(this.p0, this.p1, this.p2, this.p3);
+  @override
+  String toString() {
+    return 'ZFileEvent()';
+  }
+}
+
+class ZDataEvent implements ZModemEvent {
+  // final int p0;
+  // final int p1;
+  // final int p2;
+  // final int p3;
+  int _offset = 0;
+  int get offset => _offset;
+  ZDataEvent(int p0, int p1, int p2, int p3) {
+    _offset = (p0 << 24) | (p1 << 16) | (p2 << 8) |  (p3 << 0);
+  }
+  @override
+  String toString() {
+    return 'ZDataEvent($_offset)';
+  }
+}
+
+
 
 /// The event fired when the ZModem session is fully closed.
 class ZSessionFinishedEvent implements ZModemEvent {
